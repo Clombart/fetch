8fa2126690b42dc2c57f0490e9d1619563ecd878
Clombart <cecilia.lombart@gmail.com>
2022-07-06 00:30:24 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-d4ta8x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-d4ta8x)
---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+import './style.css';
+
+export default function App() {
+  useEffect(() => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=10';
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => console.log(data.results))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
+
+/*const gFetchApi = (url) => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => console.log(data.results))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=10';
+    gFetchApi(url);
+  }, []);
+*/
+
+//ASYNC AWAIT
+/* const getFetchApi = async (url) => {
+    try {
+      const query = await fetch(url);
+      const queryParse = await query.json();
+      console.log(queryParse.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=10';
+    getFetchApi(url);
+  }, []);
+  */
+
+//para intentar mapearlo... continuar
+
+/*const [pokemones, setPokemones] = useState([]);
+  const gFetchApi = (url) => {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => setPokemones(data.results))
+      .then((rta) => console.log(rta))
+      .catch((err) => console.log(err));
+  };
+  useEffect(() => {
+    const url = 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=10';
+    gFetchApi(url);
+  }, []);*/
